5030079a0c4391237bba61bd6012dabe36526a69
Ani <129379152+amppavai@users.noreply.github.com>
2024-02-29 13:26:47 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import TodoList from "./TodoList";
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <h1 className="centered">Simple TodoList</h1>
+      </header>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
